9b80dc3125427bfcab9937bd005fbdd0c188a2f7
Jackycna <jakycnaa@gmail.com>
2026-02-05 16:02:57 +0530

    requirementstxt added

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ app=FastAPI()
 app.mount("/assets",StaticFiles(directory="assets"),name="assets")
 app.include_router(controller)
 if __name__=="__main__":
-    uvicorn.run("main:app",host="127.0.0.1",port=9000)
+    uvicorn.run("main:app",host="0.0.0.0",port=8000,reload=True)
